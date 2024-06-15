d3991144cf4ec6f0a35fdb10cc114b098443dce3
Ferin.Chow <nrrri.cho@gmail.com>
2024-06-15 14:34:29 -0400

    first commit

---
+++ pomoList/AddEditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddEditViewController.swift
+//  pomoList
+//
+//  Created by Narissorn Chowarun on 2024-06-14.
+//
+
+import UIKit
+
+class AddEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
