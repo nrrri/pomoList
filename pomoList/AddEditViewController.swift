d61cb6cf82c49b6965a0f40a9718af44dbd82d4a
Ferin.Chow <nrrri.cho@gmail.com>
2024-07-06 16:25:11 -0400

    updated add/pdated list view

--- pomoList/AddEditViewController.swift
+++ pomoList/AddEditViewController.swift
@@ -9,21 +9,54 @@ import UIKit
 
 class AddEditViewController: UIViewController {
 
+    // create list
+    // title
+    @IBOutlet var titleView: UIView!
+    @IBOutlet var titleName: UITextField!
+    // date
+    @IBOutlet var dateView: UIView!
+    @IBOutlet var setDate: UIDatePicker!
+    // time
+    @IBOutlet var timeView: UIView!
+    @IBOutlet var timeToggle: UISwitch!
+    @IBOutlet var setTime: UIDatePicker!
+    // tag
+    @IBOutlet var tagView: UIView!
+    @IBOutlet var tags: UITextField!
+    // detail
+    @IBOutlet var detailView: UIView!
+    @IBOutlet var datail: UITextField!
+    
+    // create pomodoro
+    @IBOutlet var pomodoroView: UIView!
+    @IBOutlet var sessions: UISegmentedControl!
+    @IBOutlet var pomodoroToggle: UISwitch!
+    
+    @IBOutlet var saveBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        roundCorner()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func roundCorner() {
+        let roundCorner:CGFloat = 16
+        // round corner
+        titleView.layer.cornerRadius = roundCorner
+        dateView.layer.cornerRadius = roundCorner
+        timeView.layer.cornerRadius = roundCorner
+        tagView.layer.cornerRadius = roundCorner
+        detailView.layer.cornerRadius = roundCorner
+        pomodoroView.layer.cornerRadius = roundCorner
     }
-    */
+    
+    @IBAction func saveList(_ sender: UIButton) {
+        
+    }
+    
+    func createList(){
+        
+    }
 
 }
