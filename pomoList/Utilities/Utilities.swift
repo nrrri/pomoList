418f387600d2d349cff0c8aba4f4fc0bfd0caf9b
Ferin.Chow <nrrri.cho@gmail.com>
2024-08-03 13:24:36 -0400

    added Utilities

---
+++ pomoList/Utilities/Utilities.swift
@@ -0,0 +1,18 @@
+//
+//  Utilities.swift
+//  pomoList
+//
+//  Created by Narissorn Chowarun on 2024-08-03.
+//
+
+import SwiftUI
+
+struct Utilities: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Utilities()
+}
